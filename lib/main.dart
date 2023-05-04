3aa91360c8c21e4147030ecac2f2a1f0da867f55
Daniel Oliveira <danielh_oliveira@yahoo.com.br>
2023-05-04 18:46:11 -0300

    add botoes com sizedBox e onPressed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -35,10 +35,91 @@ class _CalculatorState extends State<Calculator> {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            // TODO: Define buttons here
+            teclado(),
           ],
         ),
       ),
     );
   }
+}
+
+Widget teclado() {
+  return Column(
+    children: [
+      Padding(
+        padding: const EdgeInsets.only(bottom: 12.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            botaoNumero("9"),
+            const SizedBox(width: 16),
+            botaoNumero("8"),
+            const SizedBox(width: 16),
+            botaoNumero("7"),
+          ],
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.only(bottom: 12.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            botaoNumero("6"),
+            const SizedBox(width: 16),
+            botaoNumero("5"),
+            const SizedBox(width: 16),
+            botaoNumero("4"),
+          ],
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.only(bottom: 12.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            botaoNumero("3"),
+            const SizedBox(width: 16),
+            botaoNumero("2"),
+            const SizedBox(width: 16),
+            botaoNumero("1"),
+          ],
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.only(bottom: 12.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            botaoNumero("0", 336.0),
+          ],
+        ),
+      ),
+    ],
+  );
+}
+
+Widget botaoNumero(String numero, [double? sizedBoxW]) {
+  if (sizedBoxW == null) {
+    return SizedBox(
+      width: 100,
+      height: 50,
+      child: ElevatedButton(
+        onPressed: () => aoClicarNumero(numero),
+        child: Text(numero),
+      ),
+    );
+  } else {
+    return SizedBox(
+      width: sizedBoxW,
+      height: 50,
+      child: ElevatedButton(
+        onPressed: () => aoClicarNumero(numero),
+        child: Text(numero),
+      ),
+    );
+  }
+}
+
+aoClicarNumero(String numero) {
+  print(numero);
 }
